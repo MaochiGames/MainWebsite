419403e5dac3ed89f2dbff1dd3e157c8e6a4fbf2
rtanjr07 <rtanjr07@gmail.com>
2026-01-25 15:17:09 +0800

    - Updating logo on landscape mobile

--- components/SiteChrome.tsx
+++ components/SiteChrome.tsx
@@ -29,7 +29,7 @@ export default function SiteChrome({ children }: SiteChromeProps) {
                 <img
                   src="/maochi-games-logo.png"
                   alt="Maochi Games"
-                  className="w-[35vw] sm:w-[10vw] md:w-[10vw] h-auto object-contain"
+                  className="w-[35vw] sm:w-[25vw] md:w-[10vw] h-auto object-contain"
                 />
               </Link>
             </div>
